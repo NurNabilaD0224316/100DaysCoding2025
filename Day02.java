f923ce96b210b0ff448b73f66f086ac1c5d3bd4e
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-09-10 11:15:11 +0800

    Day02.java

---
+++ Day02.java
@@ -0,0 +1,9 @@
+public class Day02{
+	public static void main(String[] args) {
+		int umur = 19;
+		System.out.print("Umur saya : ");
+		System.out.println(umur);
+		System.out.printf("Umur saya %d", umur);
+		
+	}
+}
