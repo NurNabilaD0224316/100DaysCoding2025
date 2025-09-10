80d5b6943dd68c61b27c7b3a4078278dd61cdb14
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-09-10 14:33:37 +0800

    Day03.java

---
+++ Day03.java
@@ -0,0 +1,9 @@
+public class Day03{
+	public static void main(String[] args) {
+		int a = 10; // Variabel a dengan nilai 10
+     int b = 3;  // Variabel b dengan nilai 3
+// Operator Aritmatika
+System.out.println(a + b); // Penjumlahan
+    
+  }
+}
