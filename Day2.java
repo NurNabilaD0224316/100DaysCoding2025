c283a76f82c4e0082b03b4bfc6c55257872ef52d
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-09-10 10:56:29 +0800

    Delete Day2.java

--- Day2.java
+++
@@ -1,8 +0,0 @@
-public class Day2 {
-	public static void main(String[] args) {
-    int umur= 19;
-     System.out.print("Umur  :");
-     System.out.println(umur);
-     System.out.printf("Umur saya %s" , umur);
-	}
-}
