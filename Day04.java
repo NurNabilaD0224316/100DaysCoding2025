cb223f949eec34c08eb45c9f469ad1dc899e82a9
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-09-11 20:01:25 +0800

    Day04.java

---
+++ Day04.java
@@ -0,0 +1,7 @@
+public class Day04 {
+    public static void main(String[] args) {
+        System.out.println("Halo\nNama\t: Nabila");
+        System.out.println("Kelas\t: informatika B");
+        System.out.println("Nim\t: D0224316 ");
+    }
+}
