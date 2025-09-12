6dc25a053a70a85fd5bffee5385a6778357bd364
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-09-12 21:16:42 +0800

    Day05.java

---
+++ Day05.java
@@ -0,0 +1,13 @@
+public class Day05 {
+    public static void main(String[] args) {
+        byte a = 100;
+        short b = 20000;
+        int c = 19;
+        long d = 8000000000L;
+
+        System.out.println("byte : " + a);
+        System.out.println("short: " + b);
+        System.out.println("int  : " + c);
+        System.out.println("long : " + d);
+    }
+}
