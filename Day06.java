5323222193ee64f6394cf4e44a9191cf7cfbe877
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-09-13 20:47:34 +0800

    Day06.java

---
+++ Day06.java
@@ -0,0 +1,9 @@
+public class Day06{
+    public static void main(String[] args) {
+        float bensin = 12.5f;      // liter
+        double jarak = 235.75;     // kilometer
+
+        System.out.println("Bensin (L)  : " + bensin);
+        System.out.println("Jarak (Km)  : " + jarak);
+    }
+}
