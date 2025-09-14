b38db86a0618363017f47a0855a7365c70703039
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-09-14 19:08:33 +0800

    Day07.java

---
+++ Day07.java
@@ -0,0 +1,13 @@
+public class Day07 {
+    public static void main(String[] args) {
+        char huruf = 'Z';      
+        char angka = '9';      
+        boolean isHuruf = true;
+        boolean isAngka = false;
+
+        System.out.println("Karakter pertama: " + huruf);
+        System.out.println("Karakter kedua: " + angka);
+        System.out.println("Apakah " + huruf + " itu huruf? " + isHuruf);
+        System.out.println("Apakah " + angka + " itu huruf? " + isAngka);
+    }
+}
