c6eecde3f207120dcc08c2f4cba425784c20f7de
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-09-15 20:14:07 +0800

    Day08.java

---
+++ Day08.java
@@ -0,0 +1,10 @@
+public class Day08{
+    public static void main(String[] args) {
+        String nama = "Nabila";
+        String jurusan = "Teknik Informatika";
+        String kampus = "Universitas Sulawesi Barat";
+        
+System.out.println("Halo, Nama saya  " + nama + ", \nJurusan\t" + jurusan + ", \nKampus\t"+ kampus);
+		
+	}
+}
