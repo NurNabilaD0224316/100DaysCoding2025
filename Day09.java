cc11c55591a88a835dea3f86471ed8b6d29125bb
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-09-16 21:08:21 +0800

    Day09.java

---
+++ Day09.java
@@ -0,0 +1,25 @@
+public class Day09 {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int umur;
+        String nama;
+
+        // Inisialisasi variabel (memberi nilai awal)
+        umur = 19;
+        nama = "Nabila";
+
+        // Cetak nilai awal
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur);
+
+        // Update nilai variabel
+        umur = 20;  
+        nama = "Nur Nabila";  
+
+        // Cetak nilai setelah diupdate
+        System.out.println("\nSetelah update:");
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur);
+    
+	}
+}
