9d46bbc10ce89cc79e03a471c102d0abc27ae49f
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-09-17 20:34:39 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,17 @@
+public class Day10 {
+    public static void main(String[] args) {
+        // Variabel biasa
+        int umur = 19;
+        System.out.println("Umur awal: " + umur);
+
+        // nilainya bisa diubah
+        umur = 20; 
+        System.out.println("Umur setelah diubah: " + umur);
+
+        // Variabel final (konstanta)
+        final double PI = 3.14159;
+        System.out.println("Nilai PI: " + PI);
+
+       
+    }
+}
