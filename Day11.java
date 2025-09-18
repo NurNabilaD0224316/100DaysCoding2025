866a7a2818ac8a11c7e8b1e29594a9889ab730ad
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-09-18 23:35:36 +0800

    Day11.java

---
+++ Day11.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Day11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nama Anda: ");
+        String nama = input.nextLine();
+        System.out.println("Halo, " + nama + "! Selamat datang.");
