8e96fb615000b81b35c5d6d450ee2a62533e3e2f
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-09-20 14:19:52 +0800

    Day13.java

---
+++ Day13.java
@@ -0,0 +1,16 @@
+public class Day13{
+    public static void main(String[] args) {
+        int umur = 20;
+        double nilai = 87.65;
+        char grade = 'A';
+        boolean lulus = true;
+        String nama = "Nabila";
+
+        System.out.printf("Nama   : %s%n", nama);
+        System.out.printf("Umur   : %d%n", umur);
+        System.out.printf("Nilai  : %.2f%n", nilai);
+        System.out.printf("Grade  : %c%n", grade);
+        System.out.printf("Lulus  : %b%n", lulus);
+    }
+}
+
