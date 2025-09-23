4a8c94f2e34d9226558a4ca3fc2cd2a5e130d5c4
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-09-23 22:07:28 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,18 @@
+public class Day16{
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 4;
+
+        // Perkalian
+        int hasilKali = a * b;
+
+        // Pembagian
+        int hasilBagi = a / b;
+
+        // Output
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Hasil Perkalian = " + hasilKali);
+        System.out.println("Hasil Pembagian = " + hasilBagi);
+    }
+}
