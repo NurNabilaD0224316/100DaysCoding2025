cd5c09da70798e917f740f64d3e1de307bd348f7
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-09-24 22:21:07 +0800

    Day17.java

---
+++ Day17.java
@@ -0,0 +1,9 @@
+public class Day17{
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 5;
+        
+        int sisa = a % b; 
+        System.out.println("Sisa bagi dari " + a + " % " + b + " = " + sisa);
+    }
+}
