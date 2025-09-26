0a70dead384a1898b795bcdcd3bff35ad4129cef
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-09-26 21:18:36 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,11 @@
+public class Day19{
+    public static void main(String[] args) {
+        int a = 130;       
+        byte b = (byte) a; 
+
+        System.out.println("int   : " + a);
+        System.out.println("byte  : " + b);
+    }
+}
+
+
