900dadc75958aae988de024f78ee992fd3c4fe92
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-09-27 22:18:48 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,12 @@
+public class Day20{
+    public static void main(String[] args) {
+        int nilai = 75;
+        double ipk = 3.85;
+
+        String strNilai = Integer.toString(nilai);
+        String strIpk = Double.toString(ipk);
+
+        System.out.println("Nilai: " + strNilai);
+        System.out.println("IPK: " + strIpk);
+    }
+}
