218bfeb796475240c59fd9f658497562425c3c76
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-09-30 20:50:07 +0800

    Day23.java

---
+++ Day23.java
@@ -0,0 +1,13 @@
+public class Day23{
+    public static void main(String[] args) {
+        int panjang = 15;  
+        int lebar = 10;      
+        int luas = panjang * lebar;
+
+        
+        System.out.println("Panjang: " + panjang);
+        System.out.println("Lebar  : " + lebar);
+        System.out.println("Luas persegi panjang: " + luas);
+    
+	}
+}
