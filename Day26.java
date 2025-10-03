bcd1d821995a191d615635d0dd9d07c485113543
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-10-03 22:08:19 +0800

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,17 @@
+public class Day26 {
+    public static void main(String[] args) {
+        int a = 20;     
+        System.out.println("Nilai awal a: " + a);
+        
+        a += 8;        
+        System.out.println("Nilai a += 8: " + a);
+        a -= 5;        
+        System.out.println("Nilai a -= 5: " + a);
+        a *= 3;        
+        System.out.println("Nilai a *= 3: " + a);
+        a /= 7;         
+        System.out.println("Nilai a /= 7: " + a);
+        a %= 4;         
+        System.out.println("Nilai a %= 4: " + a);
+    }
+}
