a7e4a69e9c2ffc1bb9abefa69fd32f0414db4746
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-10-05 19:54:57 +0800

    Day28.java

---
+++ Day28.java
@@ -0,0 +1,11 @@
+public class Day28 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int c = 5;
+
+        System.out.println("a == b: " + (a == b)); 
+        System.out.println("a == c: " + (a == c));             System.out.println("a != b: " + (a != b)); 
+        System.out.println("a != c: " + (a != c)); 
+    }
+}
