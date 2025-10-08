0a6537edeb92b0e781cc410b1a4b03a93db6d2f8
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-10-08 22:56:15 +0800

    Day31.java

---
+++ Day31.java
@@ -0,0 +1,8 @@
+public class Day31 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println((a > 0) && (b > 0));   
+        System.out.println((a > 0) && (b < 0));   
+    }
+}
