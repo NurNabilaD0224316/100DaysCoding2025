9e251ef6f2b7d12ccf352ffe8cbce23cb4fdae19
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-10-12 22:04:50 +0800

    Day35.java

---
+++ Day35.java
@@ -0,0 +1,11 @@
+public class Day35 {
+    public static void main(String[] args) {
+        int umur = 17;
+
+        if (umur >= 20) {
+            System.out.println("Kamu sudah dewasa");
+        } else {
+            System.out.println("Kamu masih remaja");
+        }
+    }
+}
