3bc4737ac2b075601aa8dcb2987e3b9d801b0f43
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-10-12 21:18:38 +0800

    Day35.java

---
+++ Day35.java
@@ -0,0 +1,13 @@
+public class Day35 {
+	public static void main(String[] args) {
+		int nilai = 60;
+
+if (nilai >= 75) {
+    System.out.println("Kamu lulus");
+} else {
+    System.out.println("Kamu belum lulus");
+}
+	}
+}
+
+	
