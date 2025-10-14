a70659288828ef8d2f8c8baba3f4f99b6e83bf3d
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-10-14 21:50:32 +0800

    Day37.java

---
+++ Day37.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Day37 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+           System.out.print("masukkan angka:");
+        int angka = input.nextInt();
+        
+        if (angka % 2 == 0) {
+            System.out.println(angka + "adalah bilangan genap");
+        } else {
+            System.out.println(angka + "adalah bilangan ganjil");
+        }
+    }
+}
