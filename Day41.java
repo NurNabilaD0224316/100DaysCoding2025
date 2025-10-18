3093a804d6198efdaf206648da27011a3f875fb4
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-10-18 21:11:50 +0800

    Day41.java

--- Day41.java
+++ Day41.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
 
-public class Day41 {
+public class Evaluasi5 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Masukkan bilangan:");
-        int bilangan = input.nextInt();
+        int A = input.nextInt();
+        int B = input.nextInt();
+        int C = input.nextInt();
+        int D = input.nextInt();
 
-        if (bilangan %3==0 && bilangan %5==0) {
-            System.out.println("master class");
-        }else if(bilangan % 3==0) {
-            System.out.println("pride of 3");
-        }else if(bilangan % 5==0) {
-            System.out.println("pride of 5");
+
+        int kiri = A*D;
+        int kanan = C*B;
+
+        if (kiri == kanan ) {
+            System.out.println("sama");
+        } else if (kiri<kanan) {
+            System.out.println("lebih kecil");
         }else {
-            System.out.println("tidak termasuk kategori");
-        }
+            System.out.println("lebih besar");
 
         }
+
+
+    }
     }
