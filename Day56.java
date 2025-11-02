2cb5b18b566787d22f31f389d1a17604a014dbfa
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-11-02 23:54:40 +0800

    Day56.java

--- Day56.java
+++ Day56.java
@@ -3,30 +3,33 @@ public class Day56 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String username;
-        int pin;
+        int nim;
         int percobaan = 0;
         boolean loginBerhasil = false;
 
         do {
             System.out.print("Masukkan Username: ");
             username = input.nextLine();
-            System.out.print("Masukkan PIN: ");
-            pin = input.nextInt();
-            input.nextLine(); 
 
-            if (username.equalsIgnoreCase("Nbila") && pin == 2804) {
+            System.out.print("Masukkan NIM: ");
+            nim = input.nextInt();
+            input.nextLine();
+
+            if (username.equalsIgnoreCase("Nbila") && nim == 224316) {
                 System.out.println("Login Sebagai Pengguna");
                 loginBerhasil = true;
-            } else if (username.equalsIgnoreCase("Admin") && pin == 9999) {
+            } else if (username.equalsIgnoreCase("Admin") && nim == D0224316) {
                 System.out.println("Login Sebagai Admin");
                 loginBerhasil = true;
             } else {
-                System.out.println("Username atau PIN salah. Silakan coba lagi!");
+                System.out.println("Username atau NIM salah. Silakan coba lagi!");
                 percobaan++;
             }
+
             if (percobaan == 3 && !loginBerhasil) {
                 System.out.println("Login Gagal! Akun anda terblokir.");
             }
+
         } while (!loginBerhasil && percobaan < 3);
     }
 }
