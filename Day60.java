9dbdba339f8bb93aca9597c6c0b2f85e3aaeb8b7
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-11-06 22:12:26 +0800

    Day60.java

---
+++ Day60.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Dsy60 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan bilangan N: ");
+        int N = input.nextInt();
+
+        int totalGenap = 0;
+        System.out.print("Genap :\n");
+        for (int i = 2; i <= N; i += 2) {
+            System.out.print(i);
+            totalGenap += i;
+            if (i + 2 <= N) {
+                System.out.print("+");
+            }
+        }
+        System.out.println(" = " + totalGenap);
+        
+        int totalGanjil = 0;
+        System.out.print("Ganjil :\n");
+        for (int i = N; i >= 1; i--) {
+            if (i % 2 != 0) {
+                System.out.print(i);
+                totalGanjil += i;
+                if (i - 2 > 0) { 
+                    System.out.print("+");
+                }
+            }
+        }
+        System.out.println(" = " + totalGanjil);
+    }
+}
