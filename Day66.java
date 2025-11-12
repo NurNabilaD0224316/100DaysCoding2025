c4e9b7c749a8797939ca30e33abe7ba07c2a47d9
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-11-12 21:49:27 +0800

    Day66.java

--- Day66.java
+++ Day66.java
@@ -3,9 +3,9 @@ public class Day66 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Masukkan Angka : ");
-        int N = input.nextInt();
+        int nn = input.nextInt();
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < nn; i++) {
             System.out.print("-"); 
         }
     }
