868852a6dc392d2beba3c34307199d7ef409f98f
NurNabilaD0224316 <nurnabilah500@gmail.com>
2025-11-13 22:19:51 +0800

    Day67.java

---
+++ Day67.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Day67 {
+    public static void main(String[] args) {
+        Scanner nn = new Scanner(System.in);
+        System.out.print("Masukkan angka : ");
+        int n = nn.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("|");
+        }
+    }
+}
